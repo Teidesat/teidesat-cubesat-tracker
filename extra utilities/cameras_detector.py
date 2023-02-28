4d5236dce7e96d6e83d21c4cc83f63691118e1a9
Sergio Tabares <alu0101124896@ull.edu.es>
2023-02-28 14:24:50 +0000

    Cameras detector output improved

--- extra utilities/cameras_detector.py
+++ extra utilities/cameras_detector.py
@@ -10,17 +10,23 @@ import cv2 as cv
 def main():
     available_ports, working_ports = detect_cameras()
 
+    print("Ignore index warnings/errors if any.", end="\n\n")
+    print("Found cameras:")
+
     for working_port, width, height, fps in working_ports:
         print(
-            f"Port {working_port} is working and reads images "
+            f"  * Port {working_port} is working and reads images "
             + f"({width}x{height} - {fps} fps)"
         )
 
     for available_port, width, height, fps in available_ports:
         print(
-            f"Port {available_port} for camera ({width}x{height} - {fps} fps) "
+            f"  * Port {available_port} for camera ({width}x{height} - {fps} fps) "
             + "is present but does not reads."
         )
+
+    if not available_ports and not working_ports:
+        print("  * No cameras found.")
 
 
 def detect_cameras():
